d367becd7225a19cb70be57b68a282b4b117ba4a
Samuel Evans-Powell <mail@sevanspowell.net>
2024-05-09 21:31:29 +0800

    haskell.nix environment

---
+++ nix/hix.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}: {
+  # name = "project-name";
+  compiler-nix-name = "ghc982"; # Version of GHC to use
+
+  # Cross compilation support:
+  crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
+    p.ghcjs
+  ]);
+
+  # Tools to include in the development shell
+  shell.tools.cabal = "latest";
+  # shell.tools.hlint = "latest";
+  # shell.tools.haskell-language-server = "latest";
+}
